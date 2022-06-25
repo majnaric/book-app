c133d92c558e21f137bebe87326271582b25430a
Gox <majnaric@gmail.com>
2022-06-25 13:20:39 +0200

    Added fictional data

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
-console.log('this is a github test')
+const axios = require('axios');
+
+{
+    "HarryPotter": {
+        "title": "Harry Potter",
+        "author": "J. K. Rowling",
+        "cover": "https://covers.openlibrary.org/b/id/5726661-M.jpg",
+        "numPages": 326,
+        "isbn": 002053150
+        ]
+    },
+    
+}
