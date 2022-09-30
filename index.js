8b8eb3a6e55e04e81fda350551cabbd2e18416a0
Gox <majnaric@gmail.com>
2022-09-30 22:26:34 +0200

    trying deploying tenth time

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/booksApp";
 
 // connecting to MongoDB
 mongoose
-  .connect('mongodb://localhost:27017/booksApp')
+  .connect(dbUrl)
   .then(() => {
     console.log("MONGO CONNECTION OPEN!");
   })
