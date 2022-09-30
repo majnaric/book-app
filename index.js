7cd37c6baf07083e85398eee4a6ea37f3145ffde
Gox <majnaric@gmail.com>
2022-09-30 22:01:07 +0200

    deleted some old files

--- index.js
+++ index.js
@@ -161,6 +161,6 @@ app.get("*", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`LISTENING ON PORT ${port}`);
 });
